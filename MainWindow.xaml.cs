66cac1fc4f0fbd6eb86127b8beabdece6c444b87
aney9 <isip_t.a.murashov@mpt.ru>
2024-04-02 11:37:38 +0300

    na 5

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace praktika3.en
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private praktikaEntities context = new praktikaEntities();
+        public MainWindow()
+        {
+            InitializeComponent();
+            dg1.ItemsSource = context.Orders.ToList();
+        }
+
+        private void dg1_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+}
